3cc21013cac995699694af9ea68b1f2e3c5e740b
Troy Sellers <tsellers@salesforce.com>
2018-08-28 16:54:41 +1000

    fix

--- bin/run.sh
+++ bin/run.sh
@@ -6,4 +6,4 @@ export BUILD_DIR=/app/sfdx
 export PATH="$BUILD_DIR/vendor/sfdx/cli/bin:$PATH"
 export PATH="$BUILD_DIR/vendor/sfdx/jq:$PATH"
 
-sfdx force:doc:commands:list
+sfdx force:mdapi:retrieve
