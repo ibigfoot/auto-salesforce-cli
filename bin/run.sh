28781744963ba2551cbe36e25adb1d039fa93bf2
Troy Sellers <tsellers@salesforce.com>
2018-09-04 14:43:22 +1000

    stuff

--- bin/run.sh
+++ bin/run.sh
@@ -6,4 +6,12 @@ export BUILD_DIR=/app/sfdx
 export PATH="$BUILD_DIR/vendor/sfdx/cli/bin:$PATH"
 export PATH="$BUILD_DIR/vendor/sfdx/jq:$PATH"
 
-sfdx force:mdapi:retrieve
+
+sfdx force:auth:jwt:grant --clientid $CLIENT_ID \ 
+    --jwtkeyfile ../server.key \
+    --username $SF_USER \
+    --setdefaultdevhubusername \
+    --setalias my-hub-org
+
+## sfdx force:mdapi:retrieve -r ../backups -u <username> -k ./package.xml
+
